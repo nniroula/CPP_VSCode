fb2c74edcbbb9e75379fd7ab5b15797ab3771de9
Nabin <nkbhaie2017@gmail.com>
2021-02-07 08:49:21 -0700

    add a function and call it from the main function

---
+++ functions.cpp
@@ -0,0 +1,13 @@
+/* Write a funciton that is called in main function. 
+*/
+#include<iostream>
+using namespace std;
+
+void myName(){
+    cout<<"Hello, my Name is Nabin Niroula\n";
+}
+int main(){
+    // call another function here
+    myName();
+    return 0;
+}
