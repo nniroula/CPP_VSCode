0352c0a87bab30588c413671483ad48f0234ebd5
Nabin <nkbhaie2017@gmail.com>
2021-02-07 11:55:56 -0700

    add a function call iside a main function

--- Functions/function_Prototype.cpp
+++ Functions/function_Prototype.cpp
@@ -28,3 +28,23 @@ int main(){
 void displayNum(int num){
     cout<< "I recied a value of "<<num<<endl;
 }
+
+/*
+This program calls a function with multple parameters inside a function main.
+*/
+#include <iostream>
+using namespace std;
+
+int getSum(int, double, int);
+int main(){
+    int num1, num3;
+    double num2;
+    cout<<"Enter the three numbers in the order"<<endl;
+    cin>>num1>>num2>>num3;
+    getSum(num1, num2, num3);
+    return 0;
+}
+int getSum(int num1, double num2, int num3){
+    //return num1 + num2 + num3;  // does help display sum on the screen
+    cout<<num1 + num2 + num3<<endl;
+}
