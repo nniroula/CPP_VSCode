4f0be5f5fae9642898ed1586744e6c71d08ff68c
Nabin <nkbhaie2017@gmail.com>
2021-02-18 19:23:10 -0700

    add vectors to store values

--- Vector/vectors.cpp
+++ Vector/vectors.cpp
@@ -9,5 +9,23 @@ int main(){
     //vector <char> places(names); // this does not work as string and char are differen data types. Following works
     vector <string> places(names); // a vector of strings called places is defined, where names is a vecor of sting already defined.
 
+/*
+This program stores, in vector, the hours worked by employees and displays those hours
+*/
+    const int number_Of_Employees = 5;
+    vector<int> hours(number_Of_Employees);
+    vector <double> payRates(number_Of_Employees);
+    int index = 0;
+    for(int i = 0; i< number_Of_Employees; i++){
+        cout<<"Please enter the hours worked by each employee "
+            <<(i + 1)<<": "; 
+        cin>>hours[i];
+    }
+    cout<<"The hours are: ";
+    for(int index = 0; index < number_Of_Employees; index++){
+        cout<<hours[index]<<" ";
+    }
+    cout<<endl;
+
     return 0;
 }
