c0ba7072d9cfb585b150557d813bca6f3478b523
Nabin <nkbhaie2017@gmail.com>
2021-03-19 17:25:52 -0600

    add second instance of the class

--- Classes/rectangleArea.cpp
+++ Classes/rectangleArea.cpp
@@ -32,20 +32,31 @@ double Rectangle::getArea() const{
 int main(){
     // create an object of a Rectangle class
      Rectangle rect1;
+     // create another instance of a class
+     Rectangle rect2;
      double length;
      double width;
      double area;
+     double area2;
 
-     cout<<"Enter the length of a rectangel: ";
+     cout<<"Enter the length of a rectangle: ";
      cin>>length;
      rect1.setLength(length);
-
      cout<<"Enter the width of the rectangle: ";
      cin>>width;
      rect1.setWidth(width);
-
      area = rect1.getArea();
+
+     // for instance 2
+     cout<<"Enter the length of a second rectangle: ";
+     cin>>length;
+     rect2.setLength(length);
+     cout<<"Enter the widht for second rectangle: ";
+     cin>>width;
+     rect2.setWidth(width);
+     area2 = rect2.getArea();
      cout<< "The are of a rectangle is "<<area<<endl;
+     cout<< "The are of a second rectangle is "<<area2<<endl;
 
     return 0;
 }
